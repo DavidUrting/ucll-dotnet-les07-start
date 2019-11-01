295c4a7c188a596d2b89c40811cfb80651671bd0
David Urting <david.urting@live.com>
2019-11-01 21:39:24 +0100

    Add project files.

---
+++ AdventureWorks.Domain/CustomerManager.cs
@@ -0,0 +1,57 @@
+﻿using AdventureWorks.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventureWorks.Domain
+{
+    public class CustomerManager
+    {
+        public IList<Customer> SearchCustomers(string keyword)
+        {
+            keyword = keyword.ToUpper();
+
+            List<Customer> customers = new List<Customer>();
+            using (var dbContext = new Entities.AdventureWorks2017Context())
+            {
+                IEnumerable<Entities.Customer> entities = dbContext.Customer
+                    .Include(c => c.Person)
+                    .Where(c => c.Person.FirstName.ToUpper().Contains(keyword)
+                                ||
+                                c.Person.LastName.ToUpper().Contains(keyword));
+                foreach (var entity in entities)
+                {
+                    customers.Add(new Customer()
+                    {
+                        ID = entity.CustomerId,
+                        FirstName = entity.Person.FirstName,
+                        LastName = entity.Person.LastName
+                    });
+                }
+            }
+            return customers;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            using (var dbContext = new Entities.AdventureWorks2017Context())
+            {
+                Entities.Customer customer = dbContext.Customer
+                    .Include(c => c.Person)
+                    .Where(c => c.CustomerId == id && c.Person != null)
+                    .FirstOrDefault();
+                if (customer != null)
+                {
+                    return new Customer()
+                    {
+                        ID = customer.CustomerId,
+                        FirstName = customer.Person.FirstName,
+                        LastName = customer.Person.LastName
+                    };
+                }
+
+            }
+            return null;
+        }
+    }
+}
